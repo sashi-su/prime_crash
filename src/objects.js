54269f180afd36195b45e833d8decaf47d2cfd53
sashi-su <152701579+sashi-su@users.noreply.github.com>
2026-06-19 21:45:31 +0900

    Refactor pointForCombo and adjust speed settings

--- src/objects.js
+++ src/objects.js
@@ -115,7 +115,19 @@ export class ScoreFloat {
 }
 
 export function pointForCombo(combo) {
-  return combo < 10 ? 10 * combo : 100;
+  if (combo === 1 || combo === 2) {
+    return 50;
+  } else if (combo === 3 || combo === 4) {
+    return 60;
+  } else if (combo === 5 || combo === 6) {
+    return 70;
+  } else if (combo === 7 || combo === 8) {
+    return 80;
+  } else if (combo === 9 || combo === 10) {
+    return 90;
+  } else if (combo >= 11) {
+    return 100;
+  }
 }
 
 export function divide(circle, square) {
@@ -152,24 +164,49 @@ export function createCircle(mode, level, numbers) {
   if (mode === 1) {
     const levelNumbers = numbers[level - 1];
     const number = levelNumbers[Math.floor(Math.random() * levelNumbers.length)];
+    // Setting of speeds for each level
     const speeds = {
-      1: 3,
-      2: 3,
-      3: 2.5,
-      4: 2.5,
-      5: 2,
-      6: 2,
-      7: 1.5,
-      8: 1.5,
-      9: 1,
+      1: 1.5,
+      2: 1.5,
+      3: 1.5,
+      4: 1.5,
+      5: 1.5,
+      6: 1.4,
+      7: 1.3,
+      8: 1.2,
+      9: 1.1,
       10: 1,
     };
     return new Circle(number, lane, speeds[level]);
   }
 
-  const speedNumbers = [...numbers[0], ...numbers[1], 57];
+  let speedNumbers;
+  if (level === 1) {
+    speedNumbers = numbers[0];
+  }
+  else if (level === 2) {
+    speedNumbers = [...numbers[0], ...numbers[0], ...numbers[0], ...numbers[1]];
+  }
+  else if (level === 3) {
+    speedNumbers = [...numbers[0], ...numbers[0], ...numbers[1]];
+  }
+  else  {
+    speedNumbers = [...numbers[0], ...numbers[1], 57];
+  }
   const number = speedNumbers[Math.floor(Math.random() * speedNumbers.length)];
-  return new Circle(number, lane, 1.5 + level * 0.5);
+  const speeds = {
+    1: 1.50,
+    2: 2.00,
+    3: 2.50,
+    4: 3.00,
+    5: 3.32,
+    6: 3.71,
+    7: 4.20,
+    8: 4.85,
+    9: 5.73,
+    10: 7.00,
+  };
+  return new Circle(number, lane, speeds[level]);
 }
 
 export function grothendieckCircles(circle) {
